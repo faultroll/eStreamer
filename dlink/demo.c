0e525f52acc70a5aa8722f9c3a2b91724a6bbf56
faultroll <boxer1992@qq.com>
2022-09-04 11:52:28 +0800

    [CHG] polish demo

--- dlink/demo.c
+++ dlink/demo.c
@@ -9,7 +9,7 @@
 
 #define ARRAY_SIZE(_arr) (sizeof(_arr) / sizeof(_arr[0]))
 
-int main(void)
+int demo_dlink(void)
 {
     typedef struct {
         DL_ID_S from;
@@ -21,21 +21,35 @@ int main(void)
         {{DL_VMUL, 0, 2}, {DL_VENC, 0, 1}},
     };
 
-    dt_init();
+    dl_init();
 
     for (i = 0; i < ARRAY_SIZE(link_tbl); ++i) {
-        dt_bind(link_tbl[i].from, link_tbl[i].to);
+        dl_link(link_tbl[i].from, link_tbl[i].to);
     }
 
     getchar();
+    dl_status();
+    getchar();
 
     for (i = 0; i < ARRAY_SIZE(link_tbl); ++i) {
-        dt_unbind(link_tbl[i].from, link_tbl[i].to);
+        dl_unlink(link_tbl[i].from, link_tbl[i].to);
     }
 
-    dt_fini();
+    dl_fini();
 
     return 0;
 }
 
+int demo_dlink_cvt(void)
+{
+    return 0;
+}
+
+int main(void)
+{
+    demo_dlink();
+    demo_dlink_cvt();
+
+    return 0;
+}
 
